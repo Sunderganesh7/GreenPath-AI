df10e9900b01637c2e97a2ad629e2e3323af6e18
Sunderganesh7 <Sunderyadavar@gmail.com>
2026-02-14 12:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -490,5 +490,9 @@ def add_header(response):
 # =====================================================
 # RUN APP
 # =====================================================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
